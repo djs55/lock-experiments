7fa40253ff651b79557cea6644e2a87dcb0a9b08
David Scott <dave.scott@citrix.com>
2015-06-11 12:28:20 +0000

    main: accept argument --uuid <uuid>
    The program will attempt to acquire the host-specific lock associated
    with <uuid>.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- main.c
+++ main.c
@@ -4,13 +4,58 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <linux/limits.h> /* for PATH_MAX */
+#include <errno.h>
 #include "lock.h"
 
-int main(int argc, char *argv[]){
-  if (argc != 2) {
-    fprintf(stderr, "Usage:\n%s <lock filename>\n\tAcquire and hold the named lock\n", argv[0]);
-    _exit(1);
+int verbose = 0;
+
+int main(int argc, char **argv) {
+  char *uuid; /* This host's uuid */
+  int c, digit_optind = 0;
+  char uuid_lock_path[PATH_MAX];
+
+  while (1) {
+    int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+      {"uuid",    required_argument, 0,  'u' },
+      {"verbose", no_argument,       0,  'v' },
+      {0,         0,                 0,  0 }
+    };
+
+    c = getopt_long(argc, argv, "u:v", long_options, &option_index);
+    if (c == -1)
+      break;
+
+    switch (c) {
+      case 'u':
+        uuid = strdup(optarg);
+        break;
+      case 'v':
+        verbose = 1;
+        break;
+      default:
+        fprintf(stderr, "unexpected return from getopt: %d\n", c);
+    }
   }
-  lock(argv[1]);
-  return 0;
+
+  if (!uuid) {
+    fprintf(stderr, "Please supply a host uuid with --uuid <uuid>\n");
+    exit(1);
+  }
+
+  if ((mkdir(".ha", 0755) == -1) && (errno != EEXIST)) {
+    perror("Failed to mkdir .ha");
+  }
+  if ((mkdir(".ha/host", 0755) == -1) && (errno != EEXIST)) {
+    perror("Failed to mkdir .hg/host");
+  }
+
+  snprintf(uuid_lock_path, sizeof(uuid_lock_path), ".ha/host/%s.lock", uuid);
+  lock(uuid_lock_path);
+  exit(0);
 }
